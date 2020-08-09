49c91c6f9d1e1f8089bd2e829c5a8523f0d910a6
Abdul Muttaqin <abdulmuttaqin456@gmail.com>
2020-08-10 00:52:53 +0700

    Delete insta.js

--- insta.js
+++
@@ -1,7 +0,0 @@
-const malScraper = require('mal-scraper')
- 
-const query = 'sakura'
- 
-malScraper.getResultsFromSearch(query)
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err))
