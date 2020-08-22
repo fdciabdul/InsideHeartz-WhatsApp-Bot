c23c39776d3da274715f4e034c825eb12c837c1b
Prakhar Gurunani <prakhargurunani@gmail.com>
2020-08-22 16:30:15 +0530

    Removed semicolon causing syntax error

--- examples/getInfo.js
+++ examples/getInfo.js
@@ -9,7 +9,7 @@ wa.searchAnime(imgPath)
   .then(searchResult => {
     return wa.getInfo(searchResult[0]['anilist_id']);
   })
-  .then(aniInfo => console.log(aniInfo));
+  .then(aniInfo => console.log(aniInfo))
   .catch(err => {
     console.error(err);
   });
